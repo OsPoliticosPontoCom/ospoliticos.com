f53fb3f845c71a245221b617022b30d3d476f6a3
Matteus S L Silva <matteus.silva@ccc.ufcg.edu.br>
2017-06-11 15:10:14 -0300

    Adding about us

---
+++ src/components/Sobre.vue
@@ -0,0 +1,78 @@
+<template lang="html">
+  <!-- #2e414d -->
+  <!-- #a63a3b -->
+<div class="container">
+  <br><br><br>
+  <img src="../assets/img/logo-blue.png">
+  <h1 class="title">Sobre</h1>
+  <div class="columns">
+    <div class="column">
+      <p class="subb">Preocupados com a dificuldade de formação de uma visão sobre a atividade dos agentes políticos da nação, desenvolvemos uma plataforma que busca concentrar informações uteis em torno da efetiva e responsável atuação dos congressistas. Seu trabalho é reger o ordenamento do país, fiscalizar o Executivo, delimitar prioridades e gastos públicos, entre outros, e o nosso, é ficar de olho neles.</p>
+    </div>
+    <div class="column">
+      <p class="subb">O nosso objetivo paira em torno da facilitação do acesso dessas informações ao público para estimular o interesse do cidadão brasileiro em sua função de fiscalizador do Estado. Remeter o poder que, do povo emana e ao povo deve servir, às suas vistas. Esperamos, de tal forma, despertar o senso crítico e a movimentação social a partir de nossa oferta.</p>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      <p class="subb">O projeto foi idealizado na 3ª edição do Hackfest – Contra a Corrupção, uma Maratona Hacker de Programação, impulsionada pelo MPPB com o tema: tecnologia, transparência e cidadania, buscando o desenvolvimento de soluções tecnológicas que estimulem a participação da sociedade no combate à corrupção. E, os desenvolvedores são: Cassio S. Cabral, Jefferson Cunha, Victor Ximenis, Matteus S. L. Silva e Ithanyê H. A. Almeida.</p>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column">
+      <h1 class="title">Agora que oferecemos o conteúdo o que se pode fazer?</h1>
+      <p class="subb">Primeiramente, é importante lembrar que através do compartilhamento nas redes sociais podemos não só divulgar as informações a população em geral, mas também reclamar por uma resposta pública dos próprios agentes políticos pelos seus canais de comunicação direta. Posteriormente, se não houver resposta ou ainda, se a resposta não atender à finalidade, podemos apresentar, diretamente no Serviço de Atendimento ao Cidadão, uma manifestação denunciativa ao Ministério Público para impulsionar o processo de investigação.</p>
+    </div>
+    <div class="column">
+      <h1 class="title">Colabore</h1>
+      <p class="subb">A finalidade do projeto é oferecer informação e estimular a consciência política, ainda que não tenhamos objetivado lucros existem custos de manutenção de domínio, despesas com servidor e realmente temos o intuito de manter a página ativa disseminando a ideia, então, se você gostou da iniciativa e quiser colaborar com o nosso projeto, aqui estão as formas de participar:</p>
+      <p class="subb">EM CONSTRUÇÃO...</p>
+      <p class="subb">Você também pode participar nos trazendo novas informações, conteúdo, curiosidades e sugestões. Entre em contato conosco aqui!</p>
+    </div>
+  </div>
+
+  <h1 class="title">Portal de Instrução</h1>
+  <div class="columns">
+    <div class="column">
+      <p class="subb">Quando se fala em governo, frontalmente se pensa nas figuras de Poder Executivo e Poder Legislativo. E, pela denominação, se forma também uma imagem, ainda que um pouco embaçada, de sua função. Dessa forma, está subentendido que o Legislativo cria as leis e o Executivo, as executa. O que não foge do contexto mas pode ser melhor esclarecido. No Brasil, e em caráter nacional, o Poder Legislativo é composto pela Câmara dos Deputados - representando os cidadãos brasileiros - e pelo Senado Federal - representando os Estados e o Distrito Federal -, e assim, forma-se o Congresso Nacional. </p>
+    </div>
+    <div class="column">
+      <p class="subb">Entre as principais funções do Poder Legislativo está, certamente, a de elaborar as leis, a de fiscalizar o Poder Executivo, votar leis orçamentárias e, em determinadas situações, julgar pessoas, incluindo o Presidente da República ou membros da Assembleia.
+O Executivo, também em caráter nacional, por sua vez, é representado pelos presidente e vice-presidente da república em exercício, são atribuídas a si, especialmente a administração federal, as relações e negócios internacionais e outras funções de responsabilidade nacional, assim como a função de sancionar ou vetar projetos de lei.</p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'sobre'
+}
+</script>
+
+<style lang="css">
+body {
+  background-color: #CCCCCC;
+}
+.title {
+  color: black;
+}
+.subb {
+  color: black;
+  font-size: 17px;
+}
+
+</style>
+
+
+<script>
+
+</script>
+
+<style lang="css">
+
+.subtitle {
+  color: white;
+}
+</style>

35f28702af3ccff65d0c7456dce0b18944840885
Cassio Cabral <cassioscabral@gmail.com>
2017-07-22 18:42:32 -0300

    atualiza perfil

--- src/components/routes/Perfil.vue
+++ src/components/routes/Perfil.vue
@@ -2,17 +2,19 @@
   <div class="perfil">
     <div class="container-fluid">
       <div class="row full-view-height">
-        <div class="col-md-2 left text-left top-space full-height">
+        <div class="col-md-3 left text-left top-space full-height">
           <div class="profile-photo">
-            <img class="img-responsive" :src="politico.urlFoto" alt="">
-            Nome: {{politico.nome}} <br>
-            Partido: {{politico.siglaPartido}}
+            <img class="img-responsive" v-if="politico.ultimoStatus" :src="politico.ultimoStatus.urlFoto" alt="">
+            Nome: {{politico.ultimoStatus.nome}} <br>
+            Partido: {{politico.ultimoStatus.siglaPartido}} <br>
+            Redes Sociais: {{politico.redeSocial}} <br>
+            Município de nascimento: {{politico.municipioNascimento}}
           </div>
         </div>
-        <div class="col-md-8 middle top-space">
+        <div class="col-md-6 middle top-space full-height">
           cards com coisas que o politico já fez, noticias, comparacoes q já fizemos, ultimos twitts
         </div>
-        <div class="col-md-2 right top-space">gráficos</div>
+        <div class="col-md-3 right top-space full-height">gráficos</div>
       </div>
     </div>
   </div>
@@ -20,6 +22,47 @@
 
 <script>
 
+//  GET https://dadosabertos.camara.leg.br/api/v2/deputados/73531
+//   {
+//   "dados": {
+//     "id": 73531,
+//     "uri": "https://dadosabertos.camara.leg.br/api/v2/deputados/73531",
+//     "nomeCivil": "IVAN VALENTE",
+//     "ultimoStatus": {
+//       "id": 73531,
+//       "uri": "https://dadosabertos.camara.leg.br/api/v2/deputados/73531",
+//       "nome": "IVAN VALENTE",
+//       "siglaPartido": "PSOL",
+//       "uriPartido": "https://dadosabertos.camara.leg.br/api/v2/partidos/36839",
+//       "siglaUf": "SP",
+//       "idLegislatura": 55,
+//       "urlFoto": "http://www.camara.leg.br/internet/deputado/bandep/73531.jpg",
+//       "data": "2015-02-01",
+//       "nomeEleitoral": "IVAN VALENTE",
+//       "gabinete": {
+//         "nome": "716",
+//         "predio": "4",
+//         "sala": "716",
+//         "andar": "7",
+//         "telefone": "3215-5716",
+//         "email": "dep.ivanvalente@camara.leg.br"
+//       },
+//       "situacao": "Exercício",
+//       "condicaoEleitoral": "Titular",
+//       "descricaoStatus": null
+//     },
+//     "cpf": "",
+//     "sexo": "M",
+//     "urlWebsite": null,
+//     "redeSocial": [],
+//     "dataNascimento": "1946-07-05",
+//     "dataFalecimento": null,
+//     "ufNascimento": "SP",
+//     "municipioNascimento": "São Paulo",
+//     "escolaridade": null
+//   },
+//   "links": null
+// }
 export default {
   name: 'perfil',
   beforeRouteEnter (to, from, next) {
@@ -33,54 +76,7 @@ export default {
       // TODO get basePath for API
       const deputado = await vm.$fetch.get(`https://dadosabertos.camara.leg.br/api/v2/deputados/${id}`)
       const result = await deputado.json()
-      vm.politico = result.dados.ultimoStatus // TODO check if dados exists first
-
-      // const maioresfornecedores = await vm.$fetch.get(`http://ops.net.br/api/deputado/${id}/maioresfornecedores`)
-      // vm.maioresfornecedores = await maioresfornecedores.json()
-      // // TODO get API basePath
-      // const maioresnotas = await vm.$fetch.get(`https://dadosabertos.camara.leg.br/api/v2/deputados/${id}/maioresnotas`)
-      // vm.maioresnotas = await maioresnotas.json()
-
-    //   {
-    //   "dados": {
-    //     "id": 73531,
-    //     "uri": "https://dadosabertos.camara.leg.br/api/v2/deputados/73531",
-    //     "nomeCivil": "IVAN VALENTE",
-    //     "ultimoStatus": {
-    //       "id": 73531,
-    //       "uri": "https://dadosabertos.camara.leg.br/api/v2/deputados/73531",
-    //       "nome": "IVAN VALENTE",
-    //       "siglaPartido": "PSOL",
-    //       "uriPartido": "https://dadosabertos.camara.leg.br/api/v2/partidos/36839",
-    //       "siglaUf": "SP",
-    //       "idLegislatura": 55,
-    //       "urlFoto": "http://www.camara.leg.br/internet/deputado/bandep/73531.jpg",
-    //       "data": "2015-02-01",
-    //       "nomeEleitoral": "IVAN VALENTE",
-    //       "gabinete": {
-    //         "nome": "716",
-    //         "predio": "4",
-    //         "sala": "716",
-    //         "andar": "7",
-    //         "telefone": "3215-5716",
-    //         "email": "dep.ivanvalente@camara.leg.br"
-    //       },
-    //       "situacao": "Exercício",
-    //       "condicaoEleitoral": "Titular",
-    //       "descricaoStatus": null
-    //     },
-    //     "cpf": "",
-    //     "sexo": "M",
-    //     "urlWebsite": null,
-    //     "redeSocial": [],
-    //     "dataNascimento": "1946-07-05",
-    //     "dataFalecimento": null,
-    //     "ufNascimento": "SP",
-    //     "municipioNascimento": "São Paulo",
-    //     "escolaridade": null
-    //   },
-    //   "links": null
-    // }
+      vm.politico = result.dados
     })
   },
   data () {
@@ -108,6 +104,10 @@ export default {
 
 .left {
   border-right: 1px solid black;
+}
+
+.right {
+  border-left: 1px solid black;
 }
 
 .top-space {
